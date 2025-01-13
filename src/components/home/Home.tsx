1d822ba6c7e6d7e10b8c0b84cc871981f73795e0
udofa18 <danieludofa452@gmail.com>
2025-01-13 19:57:12 +0100

    bbjh

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { createThirdwebClient } from "thirdweb";
 import { ConnectButton } from "thirdweb/react";
 import { darkTheme } from "thirdweb/react";
@@ -6,9 +6,11 @@ import { createWallet } from "thirdweb/wallets";
 import { AuthProvider, useAuth } from '../../utils/AuthProvider'; // Import the AuthProvider
 import { bsc } from "thirdweb/chains";
 import { useActiveWalletConnectionStatus } from "thirdweb/react";
+import { LoadingOutlined } from '@ant-design/icons';
+import { Flex, Spin } from 'antd';
 
 const client = createThirdwebClient({
-  clientId: "...",
+  clientId: "95b8094a8af9a767c30afa72aeb6b6fc",
 });
 
 const wallets = [
@@ -24,13 +26,23 @@ const Home = () => {
   const isConnected = useActiveWalletConnectionStatus();
   console.log(isConnected)
 
-
+  const [isLoading, setIsLoading] = useState(false)
+  const handleCheckEligibility = async () => {
+    setIsLoading(true);
+    // Perform your eligibility check logic here
+    // For example, you can call an API or a smart contract function
+    // Once the check is done, update the isLoading state to false
+    setIsLoading(false);
+  };
     const { doLogin, doLogout, getLoginPayload, isLoggedIn } = useAuth();
   return (
     <div className="text-center flex flex-col gap-10 justify-center m-auto">
       <div className="m-auto">
         <img src="brand/logo.svg" className="" />
       </div>
+      <p>
+      Connect a wallet to check your claim Eligibility 
+      </p>
 
       <ConnectButton
       client={client}
@@ -64,9 +76,19 @@ const Home = () => {
         await doLogout();
       }}
     />
-          {isConnected === "connected" ? (<p className="underline text-[#fb6421]">Check your Eligibility</p> ) : (
+          {isConnected === "connected" ? (
+            <button className="underline text-[#fb6421]">Check your Eligibility <Flex align="center" gap="middle">
+            {isLoading ? (
+              <Spin indicator={<LoadingOutlined spin />} /></button>  
+            ) : (
+              ""
+            )}
+          </Flex>
+          
+          ) : (
                     ""
                   )}
+                
 
     </div>
   );
